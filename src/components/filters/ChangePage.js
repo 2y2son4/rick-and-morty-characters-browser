b098c901fbb4eabb6f7337f3eaa71c9871eb6e14
2y2son4 <adrianadelteso@gmail.com>
2021-03-06 11:41:17 +0100

    Change ChangePage component semantics from div>span to nav>ul>li

--- src/components/filters/ChangePage.js
+++ src/components/filters/ChangePage.js
@@ -6,16 +6,18 @@ const ChangePage = (props) => {
   const { page, handleLess, handleMore } = props;
 
   return (
-    <nav className="arrows">
-      <span className="arrows__containerL" onClick={handleLess}>
-        <i className="fas fa-angle-double-left arrows__prev"></i>
-        <span className="arrows__text"> Previous page</span>
-      </span>
-      <span className="arrows__counter">| Page {page} |</span>
-      <span className="arrows__containerR" onClick={handleMore}>
-        <p className="arrows__text"> Next page </p>
-        <i className="fas fa-angle-double-right arrows__next"></i>
-      </span>
+    <nav>
+      <ul className="arrows">
+        <li key="left" className="arrows__containerL" onClick={handleLess}>
+          <i className="fas fa-angle-double-left arrows__prev"></i>
+          <span className="arrows__text"> Previous page</span>
+        </li>
+        <span className="arrows__counter">| Page {page} |</span>
+        <li key="right" className="arrows__containerR" onClick={handleMore}>
+          <p className="arrows__text"> Next page </p>
+          <i className="fas fa-angle-double-right arrows__next"></i>
+        </li>
+      </ul>
     </nav>
   );
 };
