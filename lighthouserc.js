4d1484e93f7f3c84a22a57fcc692136b54dd1b2a
Adriana Del Teso <adrianadelteso@gmail.com>
2022-05-22 18:13:38 +0200

    :wrench: Add config files for Lighthouse CI

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,7 +3,10 @@ module.exports = {
     collect: {
       startServerCommand: 'npm start',
       numberOfRuns: 1,
-      url: ['https://2y2son4.github.io/rick-and-morty-characters-browser/#/'],
+      url: [
+        'https://2y2son4.github.io/rick-and-morty-characters-browser/#/',
+        'https://2y2son4.github.io/rick-and-morty-characters-browser/#/page=1/character=1',
+      ],
     },
     upload: {
       target: 'temporary-public-storage',
