62c409b9e0afd77a68083c0962a5665fb270b76b
Adriana Del Teso <adrianadelteso@gmail.com>
2022-05-28 13:55:53 +0200

    :zap: Update Lighthouse CI files

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,10 +3,7 @@ module.exports = {
     collect: {
       startServerCommand: 'npm start',
       numberOfRuns: 1,
-      url: [
-        'https://2y2son4.github.io/rick-and-morty-characters-browser/#/',
-        'https://2y2son4.github.io/rick-and-morty-characters-browser/#/page=1/character=1',
-      ],
+      url: ['https://2y2son4.github.io/rick-and-morty-characters-browser/#/'],
     },
     upload: {
       target: 'temporary-public-storage',
